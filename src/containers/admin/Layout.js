965bec40f6fc4a7e68d34f7f5ed1300bacec24b9
@Sukhbold <col.de.staz@gmail.com>
2021-02-18 19:31:48 +0800

    merge fix

--- src/containers/admin/Layout.js
+++ src/containers/admin/Layout.js
@@ -10,6 +10,7 @@ import { BrowserRouter as Switch, Route } from "react-router-dom";
 import UserHandle from 'components/admin/contents/users/UserHandle';
 import ProjectHandle from 'components/admin/contents/ProjectHandle';
 import UrgudulNavigator from 'pages/urgudul/page';
+import { UrgudulStore } from 'components/utilities/urgudulContext';
 
 
 function Layout({ setLocale }) {
@@ -46,18 +47,19 @@ function Layout({ setLocale }) {
                 />
 
                 <div className="itemsPar">
-                    <Switch>
-                        <Route path="/" component={HomeAdmin} exact />
-                        <Route path="/users" component={UserHandle} />
-                        <Route path="/projects" component={ProjectHandle} />
-                        
-                        <Route path="/urgudul/:page">
-                            <UrgudulNavigator preloaded={true} />
-                        </Route>
-                    </Switch>
+                    <UrgudulStore>
+                        <Switch>
+                            <Route path="/" component={HomeAdmin} exact />
+                            <Route path="/users" component={UserHandle} />
+                            <Route path="/projects" component={ProjectHandle} />
+
+                            <Route path="/urgudul/:page">
+                                <UrgudulNavigator preloaded={true} />
+                            </Route>
+                        </Switch>
+                    </UrgudulStore>
                 </div>
             </div>
-
 
             <AlertStyle style={ctx.alert.cond === true ? { bottom: `100px`, opacity: `1`, borderLeft: `4px solid ${ctx.alert.color}` } : { bottom: `50px`, opacity: `0` }} >
                 {ctx.alert.color === "green" ? <IoMdCheckmarkCircle style={{ color: `${ctx.alert.color}` }} className="true" /> : <CgDanger style={{ color: `${ctx.alert.color}` }} className="true" />}
