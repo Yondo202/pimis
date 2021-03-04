e729aea6ecb1a8a60020e57c24ae09c192b7f277
Yondo <yondooo61@gmail.com>
2021-03-04 20:02:12 +0800

    dd

--- src/axiosbase.js
+++ src/axiosbase.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://192.168.88.78:3000/api/",
+  baseURL: "http://103.50.205.158:3000/api/",
+  // baseURL: "http://192.168.88.78:3000/api/",
   // baseURL: "http://124.158.107.34:3000/api/",
 });
 
