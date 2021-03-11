f04b2a38572522e9982141ca4d449a36bd5f9ba0
Sukhbold Altangerel <col.de.staz@gmail.com>
2021-03-11 19:28:59 +0800

    .

--- src/App.js
+++ src/App.js
@@ -53,48 +53,7 @@ function App() {
                     </Route>
                   </motion.div>
                 </Switch>
-<<<<<<< HEAD
-                //  <Route path="/admin" component={Admin} />
-              ) : (
-                <HelpStore>
-                  <Menu />
-                  <UrgudulStore>
-                    <Switch>
-                      <Route path="/" exact>
-                        <motion.div exit={{ opacity: 0 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-                          <LoginDoneHome2 />
-                        </motion.div>
-                      </Route>
-                      <Route path="/check/:url" component={CheckComp} />
-                      <Route path="/notify-page/:paramId" component={MainPage} />
-
-                      {/* <Route path="/comp-request" component={MainRequest} /> */}
-                      <Route path="/request/:url" component={MainRequestOld} />
-                      <Route path="/feedback" component={Feedback} />
-                      <Route path="/workperformance" component={WorkPerformance} />
-                      <Route path="/email2" component={EmialSender2} />
-
-                      <Route path="/members" component={MembersHome} />
-                      <Route path="/memberdecision" component={MemberDecision} />
-
-                      <Route path="/letter-of-interest" component={LetterOfInterest} />
-                      <Route path="/urgudul/:page" component={UrgudulNavigator} />
-
-                      <Route path="/attachments" component={AttachmentUploads} />
-
-                      <Route path="/5a" component={FirstEvaluation} />
-                      <Route path="/5b" component={CompilationCheck} />
-                      <Route path="/5c" component={AnalystReport} />
-
-                      <Route path="/sector-edit" component={BusinessSectorEditor} />
-                      <Route path="/product-edit" component={ProductsEditor} />
-                    </Switch>
-                  </UrgudulStore>
-                </HelpStore>
-              )
-=======
               ) : ( <UsersRoute />  )
->>>>>>> b9916d8d09a61d4b297c42fe99507c481eb733f1
                 : (<UnAuthContent />)
             }
           </Router>
