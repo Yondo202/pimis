6be163135717c9181d5cd207856f08c251a556b4
Sukhbold Altangerel <col.de.staz@gmail.com>
2021-05-28 18:48:29 +0800

    merge

--- src/axiosbase.js
+++ src/axiosbase.js
@@ -5,14 +5,8 @@ const instance = axios.create({
   // baseURL: "http://192.168.88.78:3000/api/",
   // baseURL: "http://192.168.88.231:3000/api/",
   // baseURL: "http://124.158.107.34:3000/api/",
-<<<<<<< HEAD
-  baseURL: "http://localhost:3000/api/",
-  // baseURL: "http://103.153.141.105/api/",
-  // baseURL: "https://pimis.edp.mn/api/",
-=======
   //baseURL: "http://localhost:3000/api/",
   baseURL: "https://pimis.edp.mn/api/",
->>>>>>> 3fbfd20546f6b4079526c9716283d353dba1f038
 });
 
 export default instance;
