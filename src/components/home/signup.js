80fae0387b3b168fdcea0620b6e3afd1d3275f1e
huug buug <yondooo61@gmail.com>
2021-06-17 11:11:03 +0800

    dd

--- src/components/home/signup.js
+++ src/components/home/signup.js
@@ -73,6 +73,8 @@ function Signup() {
     });
   }
 
+  console.log(`reg`, reg);
+
   const handleClick = () => {
       let rs = document.querySelectorAll(".userInp"); let arr = Array.from(rs); let finalOne = {};
       arr.forEach(element => {
@@ -85,7 +87,7 @@ function Signup() {
       let keys = Object.keys(finalOne);
       if (keys.length < 6) {
         setPassText("Гүйцэд бөгөлнө үү"); setScale("1"); setTimeout(() => { setScale("0"); setPassText(null);  }, 3000);
-      }else if(reg !== 7){
+      }else if(reg.length !== 7){
         setPassText("Регистрийн дугаараа дахин шалгана уу"); setScale("1"); setTimeout(() => { setScale("0"); setPassText(null);  }, 3000);
       }else if (selectSectors === "- Сонго -") {
         setPassText("Салбараа сонгоно уу"); setScale("1"); setShowSectors(true); setTimeout(() => { setScale("0"); setPassText(null); }, 3000);
