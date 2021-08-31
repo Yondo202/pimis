c854f5c4ff751d9a3de8596c09a228234b7e9ee4
Sukhbold Altangerel <col.de.staz@gmail.com>
2021-08-31 18:54:00 +0800

    .

--- src/axiosbase.js
+++ src/axiosbase.js
@@ -3,19 +3,10 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://103.50.205.158:3000/api/",
   // baseURL: "http://192.168.88.78:3000/api/",
-<<<<<<< HEAD
   // baseURL: "http://192.168.88.232:3000/api/",
   // baseURL: "http://124.158.107.34:3000/api/",
   baseURL: "http://localhost:3000/api/",
   // baseURL: "https://pimis.edp.mn/api/"
-=======
-  // baseURL: "http://124.158.107.34:3000/api/",
-  // baseURL: "http://localhost:3000/api/",
-
-  // baseURL: "http://192.168.88.232:3000/api/",
-
-  baseURL: "https://pimis.edp.mn/api/",
->>>>>>> 118e464dfdba93e9e4d4d3805b261bcc1f3fb7e5
 });
 
 
