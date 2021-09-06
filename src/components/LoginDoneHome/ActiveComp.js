9a4aec1aea7b55cae5676ffc53f4bd6252aed506
Sukhbold Altangerel <col.de.staz@gmail.com>
2021-09-06 16:58:21 +0800

    new build

--- src/components/LoginDoneHome/ActiveComp.js
+++ src/components/LoginDoneHome/ActiveComp.js
@@ -82,11 +82,11 @@ function ActiveComp({ userId, data }) {
             <div className="col-md-2 col-sm-2 itemsCol">
                 <div className="itemsPar">
                     <div className="mains">
-                        <Link to={`/make-contract?projectId=${projectId}`}>
+                        <Link to={projectId ? `/make-contract?projectId=${projectId}` : '/'}>
                             <div className="items arrHelp"><div className="helpArr"></div>1. Түншлэлийн гэрээ байгуулах</div>
                         </Link>
                         <div className="line"></div>
-                        <Link to={`/contract-reports?projectId=${projectId}`}>
+                        <Link to={projectId ? `/contract-reports?projectId=${projectId}` : '/'}>
                             <div className="items">2. Түншлэлийн гэрээний гүйцэтгэлийн тайлан</div>
                         </Link>
                         <div className="line"></div>
